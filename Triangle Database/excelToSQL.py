da0d7c9cb788bb755283331a470204d82835e402
Lupin Cai <lupin.cai@emory.edu>
2023-07-12 11:36:54 -0400

    fixed bugs

--- Triangle Database/excelToSQL.py
+++ Triangle Database/excelToSQL.py
@@ -6,28 +6,65 @@ import getExcel
 # DBA	CorpType	1040 type	submission date	contact name	contact number	phone	fax	email	state	county	address	fee
 class excelToSQL:
 
-    def __init__(self, data):
+    def __init__(self, data, name):
         a = getExcel
+        self.tableName = name
         self.data_frame = a.getExcel(data)
         self.table = self.create2D()
         self.keylist = self.getList()
-        self.Attribute = """
-        CREATE TABLE INFO(
-            DBA VARCHAR(100),
-            CorpType VARCHAR(10),
-            1040 type VARCHAR(20),
-            submission date DATE,
-            contact number INT,
-            fax INT,
-            email VARCHAR(50),
-            state VARCHAR(50),
-            county VARCHAR(50),
-            address VARCHAR(50),
-            fee INT);"""
 
     def getList(self):
         keyList = list(self.data_frame.map)
         return keyList
+
+    def createTable(self):
+        name = self.tableName
+        conn = pymysql.connect(host='localhost',
+                               user='root',
+                               password='',
+                               db='triangle_accounting',
+                               charset='utf8mb4',
+                               cursorclass=pymysql.cursors.DictCursor)
+
+        cursor = conn.cursor()
+        Attribute = """
+            CREATE TABLE """ + name + """(
+                DBA VARCHAR(100),
+                CorpType VARCHAR(10),
+                1040_type VARCHAR(20),
+                submission_date DATE,
+                contact_name VARCHAR(50),
+                contact_number VARCHAR(50),
+                phone VARCHAR(50),
+                fax VARCHAR(20),
+                email VARCHAR(50),
+                state VARCHAR(50),
+                county VARCHAR(50),
+                address VARCHAR(50),
+                fee INT);"""
+
+        cursor.execute(Attribute)
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+    def deleteTable(self):
+        conn = pymysql.connect(host='localhost',
+                               user='root',
+                               password='',
+                               db='triangle_accounting',
+                               charset='utf8mb4',
+                               cursorclass=pymysql.cursors.DictCursor)
+
+        cursor = conn.cursor()
+        query = "DROP TABLE IF EXISTS {}".format(self.tableName)
+
+        cursor.execute(query)
+        conn.commit()
+
+        cursor.close()
+        conn.close()
 
     def create2D(self):
         row = len(self.data_frame.state)
@@ -49,10 +86,11 @@ class excelToSQL:
         return array_2d
 
     def insertARow(self):
-        att = ''
-        for index in self.keylist:
-            att = att + index + ',' + ' '
-        att = att[:-2]
+        #        att = ''
+        #        for index in self.keylist:
+        #            att = att + index + ',' + ' '
+        #        att = att[:-2]
+        att = "DBA, CorpType, 1040_type, submission_date, contact_name, contact_number, phone, fax, email, state, county, address, fee"
 
         conn = pymysql.connect(host='localhost',
                                user='root',
@@ -81,25 +119,24 @@ class excelToSQL:
             fee = self.data_frame.fee[index]
 
             # swap the actual name for info, the same as above with create table
-            cursor.execute("INSERT INTO INFO (" + att + ") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (
+            query = "INSERT INTO {} ({}) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)".format(
+                self.tableName,
+                att)
+
+            # Execute the SQL query with the values
+            cursor.execute(query, (
                 DBA, CorpType, tenFortyType, subDate, conName, conNum, phone, fax, email, state, county, address, fee))
 
-            conn.commit()
+        conn.commit()
 
-            cursor.close()
-            conn.close()
+        cursor.close()
+        conn.close()
 
 
 if __name__ == '__main__':
     data_frame = pd.read_excel('test.xlsx')
-    B = excelToSQL(data_frame)
-    array_2d = B.table
+    b = excelToSQL(data_frame, "info")
+    #b.deleteTable()
+    b.createTable()
+    b.insertARow()
 
-    for index in array_2d:
-        print(' '.join(map(str, index)))
-
-    att = ''
-    for index in B.keylist:
-        att = att + index + ',' + ' '
-    att = att[:-2]
-    print(att)
